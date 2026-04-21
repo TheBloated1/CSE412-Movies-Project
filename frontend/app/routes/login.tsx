8f36a8a895360cfec352a1f557e0d3ff637aaea7
Michael Jonathan Hawes <mkster16@gmail.com>
2026-04-20 19:57:07 -0700

    small changes

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -6,8 +6,12 @@ export function loader() {
 
 export default function Login({ loaderData }: Route.ComponentProps) {
   return (
-    <div className="h-screen flex items-center justify-center">
-        Ello!
+    <div className="bg-gray-500 h-screen flex items-center justify-center">
+        <div className="bg-gray-200 p-6 flex flex-col gap-3 w-80">
+            <input type="text" placeholder="Enter your username" className="pl-2"></input>
+            <input type="password" placeholder="Enter your password" className="pl-2"></input>
+            <button className="hover:bg-gray-700 px-3 py-2 rounded-full">Input</button>
+        </div>
     </div>
   );
 }
